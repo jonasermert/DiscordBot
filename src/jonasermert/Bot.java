b4c3a63e17ea88d8bf9269f25a10cf9da25ccf11
Jonas Ermert <ermert.jonas@yahoo.de>
2021-08-24 09:51:48 +0200

    sendInvitatio() and createServer() Method implemented

--- src/jonasermert/Bot.java
+++ src/jonasermert/Bot.java
@@ -3,6 +3,12 @@ package com.github.jonasermert
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
+/**
+ * @author Jonas Ermert
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
 public class Bot {
 
     public static void main(String[] args) {
@@ -17,6 +23,8 @@ public class Bot {
         });
 
         createChannel();
+        sendInvitation();
+        createServer();
 
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
     }
@@ -27,5 +35,24 @@ public class Bot {
                 .setUserlimit(10)
                 .create().join();
     }
+
+    public sendInvitation(){
+        Invite invite = new InviteBuilder(channel)
+                .setMaxAgeInSeconds(60*60*24)
+                .setMaxUses(42)
+                .create().join();
+    }
+
+    public createServer(){
+        long serverId = new ServerBuilder(api)
+                .setName("Jonas Server")
+                .setIcon(api.getYourself().getAvatar())
+                .setVerificationLevel(VerificationLevel.HIGH)
+                .setDefaultMessageNotificationLevel(DefaultMessageNotificationLevel.ONLY_MENTIONS)
+                .setRegion(Region.EU_CENTRAL)
+                .create().join();
+    }
+
+
 }
 
