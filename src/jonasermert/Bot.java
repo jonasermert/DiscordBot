cdf3ba625e7d994aad7611f5ba1527fc105a64df
Jonas Ermert <ermert.jonas@yahoo.de>
2021-08-24 09:42:46 +0200

    createChannel Method implemented and called in main()

--- src/jonasermert/Bot.java
+++ src/jonasermert/Bot.java
@@ -11,17 +11,21 @@ public class Bot {
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
         api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-                event.getChannel().sendMessage("Hallo, ich bin Jonas Discord Bot");
+            if (event.getMessageContent().equalsIgnoreCase("!jonas")) {
+                event.getChannel().sendMessage("Hallo, ich bin Jonas Discord Bot", MessageDecoration.BOLD);
             }
         });
 
+        createChannel();
+
         System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+
+    public createChannel(Server server) {
+        ServerVoiceChannel channel = new ServerVoiceChannelBuilder(server)
+                .setName("jonas-channel")
+                .setUserlimit(10)
+                .create().join();
     }
 }
 
-
-
-
-
-}
